1e4105f36fd0309aac3453a1445c72f02ceeab69
Nina Matthews <ninaematthews@gmail.com>
2021-11-06 20:03:56 +0200

    Working on Turnover

--- All_Ports_Rolling_Windows.R
+++ All_Ports_Rolling_Windows.R
@@ -505,16 +505,28 @@ legend("topleft",c(names(G_tsALL)[1:5], "ALSI (Equity)", "ALBI (Bonds)"),col = c
 ## Storage for End of Month Weights
 # 1. EW
 Overlap_tsE_WtsEnd   <- tsGRet* 0 # for i-th month
-# 2. 
+# 2. SR
 Overlap_tsSR_WtsEnd   <- tsGRet* 0 # for i-th month
 # 3. BH
 ## Already have Wts calculated: Overlap_tsBH_WtsEnd
 # 4. HRP
 Overlap_tsHRP_WtsEnd   <- tsGRet* 0 # for i-th month
 # 5. Constant Mix
-Overlap_tsCM_WtsEnd   <- tsGRet* 0 # for i-th month
+Overlap_tsCM_WtsEnd   <- tsCM* 0 # for i-th month
 
-## Storage for adjusted ports for trading costs
+## Storage for Monthly % cost p/Portfolio
+#1. Equal 
+Overlap_tsE_TOver <- tsGRet[,1]*0 
+#2. SR 
+Overlap_tsSR_TOver <- tsGRet[,1]*0 
+#3. BH 
+Overlap_tsBH_TOver <- tsGRet[,1]*0 
+#4. HRP 
+Overlap_tsHRP_TOver <- tsGRet[,1]*0 
+#5. CM 
+Overlap_tsCM_TOver <- tsCM[,1]*0 
+
+## Storage for adjusted ports realised returns for trading costs
 # 1. EW
 Overlap_tsE_adj   <- tsGRet[,1]*0 
 # 2. 
@@ -524,7 +536,7 @@ Overlap_tsBH_adj  <- tsGRet[,1]*0
 # 4. HRP
 Overlap_tsHRP_adj   <- tsGRet[,1]*0 
 # 5. Constant Mix
-Overlap_tsCM_adj <- tsGRet[,1]*0 
+Overlap_tsCM_adj <- tsCM[,1]*0 
 
 # loop deals with a single month at a time starting with month (Window)
 for (i in Window:(tot[1]-1)){
@@ -539,36 +551,49 @@ for (i in Window:(tot[1]-1)){
         #4. HRP
         Overlap_tsHRP_WtsEnd[i,] <- (tsGRet[i,]*Overlap_HRP_Wts[i,])+Overlap_HRP_Wts[i,]
         #5. Constant Mix
-        Overlap_tsCM_WtsEnd[i,] <- (tsGRet[i,]*CMWts)+CMWts
+        Overlap_tsCM_WtsEnd[i,] <- (tsCM[i,]*CMWts)+CMWts
+        
+        ######## P Turnover per month ## ISSUE LIES HERE
+        #1. Equal 
+        Overlap_tsE_TOver[i] <- apply(abs(Overlap_tsE_WtsEnd - EWts),1,sum)
+        #2. SR
+        Overlap_tsSR_TOver[i] <- apply(abs(Overlap_tsSR_WtsEnd - Overlap_tsWts),1,sum)
+        #3. BH
+        Overlap_tsBH_TOver[i] <- apply(abs(Overlap_tsBH_WtsEnd - Overlap_tsBH_Wts0),1,sum)
+        #4. HRP
+        Overlap_tsHRP_TOver[i] <- apply(abs(Overlap_tsHRP_WtsEnd - Overlap_HRP_Wts),1,sum)
+        #5. CM
+        Overlap_tsCM_TOver[i] <- apply(abs(Overlap_tsCM_WtsEnd-CMWts),1,sum)
+        
+        
+        # #### Percentage cost of rebalancing using 35 basis points p/mnth
+        #1.  Equal
+        Overlap_tsE_cost[i] <- 0.0035* Overlap_tsE_TOver[i]
+        #2. SR
+        Overlap_tsSR_cost[i] <- 0.0035* Overlap_tsSR_TOver[i]
+        # 3. BH
+        Overlap_tsBH_cost[i] <- 0.0035* Overlap_tsBH_TOver[i]
+        #4. HRP
+        Overlap_tsHRP_cost[i] <- 0.0035* Overlap_tsE_TOver[i]
+        #5. Constant Mix
+        Overlap_tsCM_cost[i] <- 0.0035* Overlap_tsCM_TOver[i]
 
-        ####### Adjusting returns  
+        ####### Adjusting returns p/mnth
         #1. Equally weighted realised returns
-        Overlap_tsERet[i] <- sum((EWts*(tsGRet[i,]))-(0.0035*abs(Overlap_tsE_WtsEnd[i,]-EWts)))
+        Overlap_tsE_adj[i] <- Overlap_tsERet[i] - Overlap_tsE_cost[i]
         
         #2 SR realised returns
-        Overlap_tsSR_adj[i]  <- (Overlap_tsWts[i,] %*% t(tsGRet[i,])-(0.0035*(Overlap_tsSR_WtsEnd[i,]-Overlap_tsWts[i,])))
+        Overlap_tsSR_adj[i]  <- Overlap_tsPRet[i] - Overlap_tsSR_cost[i]
         
         #3. BH realised returns
         ## Realised Port returns (Sum across all assets) 
-        Overlap_tsBH_adj[i] <- Overlap_tsBH_Wts0[i,] %*% t(tsGRet[i,]-(0.0035*(Overlap_tsBH_WtsEnd[i,]-Overlap_tsBH_Wts0[i,])))
+        Overlap_tsBH_adj[i] <- Overlap_tsBHRet[i] - Overlap_tsBH_cost[i]
         
         #4. HRP realised returns
-        Overlap_tsHRP_adj[i] <- Overlap_HRP_Wts[i,] %*% t(tsGRet[i,]-(0.0035*(Overlap_tsHRP_WtsEnd[i,]-Overlap_HRP_Wts[i,])))
+        Overlap_tsHRP_adj[i] <- Overlap_HRP_PRet[i] - Overlap_tsHRP_cost[i]
         
         #5. CM realised returns
-        Overlap_tsCM_adj[i] <- CMWts %*% t(tsGRet[i,]-(0.0035*(Overlap_tsCM_WtsEnd[i,]-CMWts)))
-        
-        # #### Percentage cost of rebalancing using 35 basis points
-        # #1.  Equal
-        # Overlap_tsE_adj[i] <- Overlap_tsERet[i]-(0.0035*sum(Overlap_tsE_WtsEnd[i,]-EWts))
-        # #2. SR
-        # Overlap_tsSR_adj[i] <- Overlap_tsPRet[i]-(0.0035*sum(Overlap_tsSR_WtsEnd[i,]-Overlap_tsWts[i,]))
-        # # 3. BH 
-        # Overlap_tsBH_adj[i] <- Overlap_tsBHRet[i]-(0.0035*sum(Overlap_tsBH_WtsEnd[i,]-Overlap_tsBH_Wts0[i,]))
-        # #4. HRP
-        # Overlap_tsHRP_adj[i] <- Overlap_HRP_PRet[i]-(0.0035*sum(Overlap_tsHRP_WtsEnd[i,]-Overlap_HRP_Wts[i,]))
-        # #5. Constant Mix
-        # Overlap_tsCM_adj[i] <- Overlap_tsCMRet[i]-(0.0035*sum(Overlap_tsCM_WtsEnd[i,]-CMWts))
+        Overlap_tsCM_adj[i] <- Overlap_tsCMRet[i] - Overlap_tsCM_cost[i]
 }
 
 
@@ -607,15 +632,12 @@ legend("topleft",names(O_adj_tsALL),col = c("orange","magenta","blue","green", "
 # DO TO list:
 
 ### NINA
-# 0. link git (DONE)
-# 1. Need to change BH to constant mix portfolio (DONE)
-# 2. Add proper BH port (DONE)
-# 3. Compute and plot turnovers 
+# 1. Compute and plot turnovers 
 
 ### SIPHE
-# 1. Geometric Returns
-# 2. Winzorise
-# 3. Covar
+# 1. Deflated SR
+# 2. PBO
+# 3. 
 
 
 ### Relative mix changes over time depending on how the real asset classes in relative perform
